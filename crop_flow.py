6840253ef146b099dfb809572490d749ae344622
Ruoteng Li <liruoteng@gmail.com>
2016-10-20 19:22:37 +0800

    add crop_flow.py

--- crop_flow.py
+++ crop_flow.py
@@ -31,23 +31,29 @@ if not args.flow_format:
 file_list = []
 in_dir = os.path.abspath(args.input_dir)
 out_dir = os.path.abspath(args.output_dir)
+flow_count = 0
+image_count = 0
 for root, dirs, files in os.walk(in_dir):
     if files:
         for filename in files:
             file_path = os.path.join(in_dir, root + '/' + filename)
             if filename.find(args.flow_format) != -1:
                 # this is flow file
+                image_count += 1
+                print 'image count: ', image_count
                 flow = fl.read_flow(file_path)
                 new_flow = flow[args.y-1:args.y+args.h-1, args.x-1:args.x-1+args.w, :]
-                out_path = os.path.join(out_dir, 'flow')
+                out_path = os.path.join(out_dir, root[root.find('drive/')+6:])
                 if not os.path.exists(out_path):
                     os.makedirs(out_path)
                 fl.write_flow(new_flow, os.path.join(out_path, filename))
             elif filename.find(args.image_format) != -1:
                 # this is an image file
+                flow_count += 1
+                print 'flow count: ', flow_count
                 img = Image.open(file_path)
                 new_img = img.crop((args.x-1, args.y-1, args.x+args.h-1, args.y+args.w-1))
-                out_path = os.path.join(out_dir, 'image')
+                out_path = os.path.join(out_dir, root[root.find('drive/')+6:])
                 if not os.path.exists(out_path):
                     os.makedirs(out_path)
                 new_img.save(os.path.join(out_path, filename))
